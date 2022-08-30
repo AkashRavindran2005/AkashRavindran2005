22ad4d286d1d768ed787320ed56b0c350b43c61e
Akash <ironmanak0205@gmail.com>
2022-08-30 17:45:13 +0530

    amen

--- invoice/Invioce.py
+++ invoice/Invioce.py
@@ -21,7 +21,7 @@ def bill():
             invoice += str(today) + '-' + '0' + str(no)
         elif no >= 100:
             invoice += str(today) + '-' + str(no)
-        with open('Invoice.csv', 'a', newline='') as f:
+        with open('../Invoice.csv', 'a', newline='') as f:
             sr = csv.writer(f, delimiter = ' ')
             s = [
                 ['                          Annachi Kadai                      '],
@@ -33,7 +33,7 @@ def bill():
             sr.writerows(s)
             st = ["{:<5} {:<17} {:<15} {:<5} {:<8} {:<8}".format('S.no', 'Id', 'Description', 'QTY', 'MRP', 'Amount')]
             sr.writerow(st)
-            with open('products.csv', 'r', newline='') as f1:
+            with open('../products.csv', 'r', newline='') as f1:
                 reader = csv.reader(f1)
                 print('{:<100} {:<5}'.format('Products', 'Stocks'))
                 n=[]
@@ -60,7 +60,7 @@ def bill():
                     if found == 0:
                         print('not found')
                     else:
-                        with open('products.csv','w',newline='') as f2:
+                        with open('../products.csv', 'w', newline='') as f2:
                             writer = csv.writer(f2)
                             writer.writerows(t)
                     price = int(input('Enter product price: '))
@@ -133,13 +133,13 @@ def order(t):
                 print('Placed order for',i[0])
                 i[1] = str(100)
         f.append(i)
-    with open('products.csv','w',newline='') as f1:
+    with open('../products.csv', 'w', newline='') as f1:
         writer = csv.writer(f1)
         writer.writerows(f)
 
 
 def read():
-    with open('Invoice.csv','r',newline='') as f:
+    with open('../Invoice.csv', 'r', newline='') as f:
         sr=csv.reader(f)
         for i in sr:
             for j in i:
