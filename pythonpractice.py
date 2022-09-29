00b7c158df9549e60f1f6f39ca8f3a5ca4725a98
Akash <ironmanak0205@gmail.com>
2022-09-29 19:20:35 +0530

    afdda

--- pythonpractice.py
+++ pythonpractice.py
@@ -561,15 +561,3 @@ list = [23, 1, 31, 106, 3]
 # label = tkinter.Label(root,text='Akash')
 # label.pack()
 # root.mainloop()
-
-from chatterbot import chatbot
-from chatterbot.trainers import ListTrainer
-
-# creating a new chatbot
-chatbot = Chatbot('Edureka')
-trainer = ListTrainer(chatbot)
-trainer.train(['hi, can I help you find a course', 'sure I'd love to find you a course', 'your course have been selected'])
-# getting a response from the chatbot
-response = chatbot.get_response("I want a course")
-print(response)
-import s
